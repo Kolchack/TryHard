d4f1af641e580dabc9af555d5b7f6e9d58db4a41
Alexandr Kolchin <alexandr-kolchin@mail.ru>
2016-02-24 03:13:54 +0300

    frame

--- FirstApp/src/Head.java
+++ FirstApp/src/Head.java
@@ -1,16 +1,30 @@
 import javax.swing.*;
+import java.awt.*;
 
-/**
- * Created by Alexadnr on 24.02.2016.
- */
+
 public class Head {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
         JFrame frame = new JFrame("Frame");
         frame.setSize(640,480);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        frame.setLayout(new GridBagLayout());
         frame.setLocationRelativeTo(null);
+        JMenuBar bar = new JMenuBar();
+        frame.add(bar);
+        JProgressBar pb =new JProgressBar();
+        frame.add(pb);
+        pb.setMinimum(0);
+        pb.setMaximum(100);
+        JMenu menu =new JMenu("menu");
+
+        bar.add(menu);
+        frame.setVisible(true);
+        for (int i=0;i<100;i++){
+            pb.setValue(i);
+            Thread.sleep(300);
+        }
+
     }
 
 }
