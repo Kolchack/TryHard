74fa8835de4d99ffa04c48c445386936c91eda80
Alexandr Kolchin <alexandr-kolchin@mail.ru>
2016-02-24 03:04:04 +0300

    frame

--- FirstApp/src/Head.java
+++ FirstApp/src/Head.java
@@ -1,5 +1,16 @@
+import javax.swing.*;
+
 /**
  * Created by Alexadnr on 24.02.2016.
  */
 public class Head {
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Frame");
+        frame.setSize(640,480);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+    }
+
 }
